15b4679f250dbe584bb130196dfe52376c6d3e91
Jonathan <Ore>
2020-07-16 08:52:28 -0400

    Upgraded angular cli and core versions, moved configuration files to assets directory.

--- src/app/configurations/app-config/app.config.ts
+++ src/app/configurations/app-config/app.config.ts
@@ -13,11 +13,11 @@ export class AppConfig {
         const jsonFile = `app/configurations/app-config/config.${environment.name}.json`;
         return new Promise<void>((resolve, reject) => {
             this.http.get(jsonFile).toPromise().then((response: IAppConfig) => {
-                AppConfig.settings = <IAppConfig>response;
+                AppConfig.settings = response as IAppConfig;
                 resolve();
             }).catch((response: any) => {
                 reject(`Could not load file '${jsonFile}': ${JSON.stringify(response)}`);
-            })
-        })
+            });
+        });
     }
 }
